4d48a32a7137e10a4d63572b75661e5e749800f9
Gustavo Máximo <gfmaximo97@gmail.com>
2019-07-17 22:26:18 -0300

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return ( 
+            <>
+            <div>Howdy,</div>
+            <p>Name&#39;s Máximo. I&#39;m trying to make development my second nature</p>
+            </>
+        )
+    }
 }
 
 export default App;
